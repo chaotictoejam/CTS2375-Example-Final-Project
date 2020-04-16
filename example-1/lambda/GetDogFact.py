1caef5c8ef1f2782a59260a7541f21788d4176b7
Joanne <siroisjo@gmail.com>
2020-04-16 10:52:24 -0400

    Updated GetDogFact and added images.

--- example-1/lambda/GetDogFact.py
+++ example-1/lambda/GetDogFact.py
@@ -1,14 +1,24 @@
 import json
+import random
 
 def lambda_handler(event, context):
-    # TODO implement
+    factlist = [["Dogs like to play in water!", "images/leela_water.jpg"], 
+        ["Dogs don't understand beds are for sleeping.", "images/leela_bed.jpg"],  
+        ["Dogs destroy there toys", "images/leela_toys.jpg"],  
+        ["Wear sweaters. Seriously? You have fur.", "images/leela_sewaters.jpg"],  
+        ["Attack the broom.", "images/leela_broom.gif"],  
+        ["Like to play in dirt.", "images/leela_dirt.jpg"]
+    ]
+    
+    randomFact = random.choice(factlist);
+    
     return {
         'statusCode': 200,
         'headers': {
             "Access-Control-Allow-Orgin" : "*"
         },
         'body': {
-            'fact':'This is a random dog fact',
-            'image':'images/leela.jpg'
+            'fact':randomFact[0],
+            'image':randomFact[1]
         }
     }
