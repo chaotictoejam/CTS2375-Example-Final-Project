57e8ba09cc955a952d95b4a7876b4af625eaf767
Joanne <siroisjo@gmail.com>
2020-04-16 11:03:04 -0400

    Updated GetCatFact

--- example-1/lambda/GetCatFact.py
+++ example-1/lambda/GetCatFact.py
@@ -1,14 +1,24 @@
 import json
+import random
 
 def lambda_handler(event, context):
-    # TODO implement
+    factlist = [["Use sustainable energy to stay warm.", "images/algebra_sunbeam.jpg"], 
+        ["Cats are resourceful, we can make anything a bed", "images/algebra_backpack.jpg"],  
+        ["Cats can make themselves compact.", "images/chell_ball.jpg"],  
+        ["Can climb up high", "images/brr_high.jpg"],  
+        ["Cats know how to use a bed.", "images/algebra_bed.jpg"],  
+        ["Cats are perfect.", "images/chell2.jpg"]
+    ]
+    
+    randomFact = random.choice(factlist);
+    
     return {
         'statusCode': 200,
         'headers': {
             "Access-Control-Allow-Orgin" : "*"
         },
         'body': {
-            'fact':'This is a random fact',
-            'image':'images/chell2.jpg'
+            'fact':randomFact[0],
+            'image':randomFact[1]
         }
     }
